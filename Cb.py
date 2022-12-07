f050dc5cfb3ac859ac8bfa384ca1238f42d39a6c
MausmiS <113102214+MausmiS@users.noreply.github.com>
2022-12-07 23:16:28 +0530

    Add files via upload

---
+++ Cb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# returns the cube of any given number
+def cubeofnumber(num):
+    return num*num*num
+
+
+# In[8]:
+
+
+cubeofnumber(5)
+
+
+# In[ ]:
+
+
+
+
